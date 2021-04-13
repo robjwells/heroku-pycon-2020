4389d4b855554a7efd078e2b15a5b8d05016c2d2
Rob Wells <rob@robjwells.com>
2021-04-13 11:23:49 +0100

    Allow 'localhost' in base settings

--- gettingstarted/settings/base.py
+++ gettingstarted/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be us
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
